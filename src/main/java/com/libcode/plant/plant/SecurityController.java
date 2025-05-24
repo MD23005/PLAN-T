ada73fad225e56fef88b396f9666b03a4211431b
Ronald Manzano <md23005@ues.edu.sv>
2025-05-24 15:44:43 -0600

    Cambios a los controladores y al pom.xml

--- src/main/java/com/libcode/plant/plant/SecurityController.java
+++ src/main/java/com/libcode/plant/plant/SecurityController.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityController {
@@ -17,14 +19,16 @@ public class SecurityController {
                 .requestMatchers("/", "/index", "/login/**", "/css/**", "/js/**","/img/**").permitAll() // 👈 Acceso libre
                 .anyRequest().authenticated()
             )
-            .formLogin(form -> form
+             .oauth2Login(withDefaults());
+             
+            /*.formLogin(form -> form
                 .loginPage("/login") // Opcional: tu vista personalizada
                 .permitAll()
             )
             .logout(logout -> logout
                 .logoutSuccessUrl("/")
                 .permitAll()
-            );
+            );*/
 
         return http.build();
     }
