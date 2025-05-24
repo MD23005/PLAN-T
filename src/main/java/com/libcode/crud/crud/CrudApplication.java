f0bb6e423ea37d917e5cf85282891a0a4bea2e0c
ÁngelCG23016 <angelcardoza503@gmail.com>
2025-05-23 19:52:53 -0600

    Actualizacion

--- src/main/java/com/libcode/crud/crud/CrudApplication.java
+++ src/main/java/com/libcode/crud/crud/CrudApplication.java
@@ -2,20 +2,12 @@ package com.libcode.crud.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-<<<<<<< HEAD
-=======
 import com.libcode.crud.crud.config.EnvLoader;
 
->>>>>>> c54c1dc (Variables de entorno)
 @SpringBootApplication
 public class CrudApplication {
-
 	public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-		EnvLoader.loadEnv();
->>>>>>> c54c1dc (Variables de entorno)
+	    EnvLoader.loadEnv();
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
