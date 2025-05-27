bac47da22f4c6a1979dca77f9b43b1028149788e
Ronald Manzano <md23005@ues.edu.sv>
2025-05-26 20:30:05 -0600

    resolviendo conflictos surgidos durante el merge

--- src/main/java/com/libcode/plant/plant/LogoutController.java
+++ src/main/java/com/libcode/plant/plant/LogoutController.java
@@ -8,26 +8,9 @@ public class LogoutController {
 
     @GetMapping("/custom-logout")
     public String logout() {
-        String returnTo = "http://localhost:8080/";
+        String returnTo = "http://https://plan-t-rol7.onrender.com/";
         String clientId = "sbj7qQLLPsUFrNliyCs6ZmDZ8olbDP99";
         String logoutUrl = "https://dev-qi8b5nrabbvawh0y.us.auth0.com/v2/logout?client_id=" + clientId + "&returnTo=" + returnTo;
-        return "redirect:" + logoutUrl;
-    }
-}
-
-package com.libcode.plant.plant;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class LogoutController {
-
-    @GetMapping("/custom-logout")
-    public String logout() {
-        String returnTo = "http://localhost:8080/";
-        String clientId = "FvGwy7Qw9bPiTFSbHGI3IfEwJQVzX8n8";
-        String logoutUrl = "https://dev-wc3lul6afa5rcq0p.us.auth0.com/v2/logout?client_id=" + clientId + "&returnTo=" + returnTo;
         return "redirect:" + logoutUrl;
     }
 }
